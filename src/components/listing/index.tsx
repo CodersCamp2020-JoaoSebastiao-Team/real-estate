97be77112331c3c42399265920db2b4f9294b02d
Adrian Kotlinski <adrian.kotlinski@script-studio.pl>
2021-03-26 13:07:40 +0100

    created basic component for listing

---
+++ src/components/listing/index.tsx
@@ -0,0 +1,12 @@
+import './index.scss';
+import React from 'react';
+
+
+const Listing = () => {
+    return (
+      <div className="listing-wrapper">
+        <h3>Listing</h3>
+      </div>
+    );
+  };
+  export default Listing;
--- src/pages/buy.tsx
+++ src/pages/buy.tsx
@@ -1,7 +1,12 @@
+import Listing from '../components/listing'
+
 const Buy = () => {
   return(
     <div>
       <h2>Buy page</h2>
+      <Listing />
+      <Listing />
+      <Listing />
     </div>
   );
 };
