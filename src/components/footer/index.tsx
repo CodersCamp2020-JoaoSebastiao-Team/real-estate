81da03a781fcb95e87be9b798db13f7702192b12
Adrian Kotlinski <adrian.kotlinski@script-studio.pl>
2021-03-26 12:38:50 +0100

    Add social media to footer - created responsive version of footer for mobile devices

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,26 @@
 import './index.scss';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import { FaInstagram,FaFacebookSquare, FaLinkedin , FaAt   } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
+      <CookieConsent
+        enableDeclineButton
+        flipButtons
+        location="bottom"
+        buttonText="Accept Cookies"
+        cookieName="Accept_cookies"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={1}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>Accept cookies to get better experience!</span>
+      </CookieConsent>
       <div className="footer-head">
         <ul>
           <Link to="/about"><li className="line-item">About us</li></Link>
@@ -12,17 +28,19 @@ const Footer = () => {
           <Link to="/contact"><li className="line-item">Contact</li></Link>
         </ul>
       </div>
-      <div>
-      <ul className="line-item footer-left">
+      <div className="footer-body">
+        <ul className="line-item footer-left">
           <li>@2021 Real Esate App - All right reserved.</li>
           <Link to="/privacy"><li className="line-item">Privacy policy & processing personal data</li></Link>
-      </ul>
-      <ul className="line-item footer-right">
-          <li>Cookies</li>
-          <li>FB</li>
-          <li>Insta</li>
-
-      </ul>
+        </ul>
+        <ul className="line-item footer-right">
+        <IconContext.Provider value={{ size: "2em" ,className: "social-icons" }}>
+          <li><a href="https://www.instagram.com/coderscrew.pl/" target="_blank"><FaInstagram /></a></li>
+          <li><a href="https://www.facebook.com/ccrew18/" target="_blank"><FaFacebookSquare /></a></li>
+          <li><a href="https://www.linkedin.com/company/coderscrew/" target="_blank"><FaLinkedin /></a></li>
+          <li><a href="mailto:kontakt@coderscrew.pl"><FaAt /></a></li>
+          </IconContext.Provider>
+        </ul>
       </div>
     </div>
   );
