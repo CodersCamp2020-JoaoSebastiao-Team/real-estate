f6b427bd1856cf25a0ded470ae71bc1c4f2f759b
Magdalena <sochamagdalena8@gmail.com>
2021-03-27 23:25:28 +0100

    style changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import Listing from '../components/listing'
 import listingProps from '../components/listing/listingProps'
 import Hero from '../components/hero'
 import CardsSection from '../components/card-section'
+import '../styles/partials/_typography.css'
 
 
 const myProps: listingProps = { width: "300px", height: "202px", url: "https://gratka.pl/blog/wp-content/uploads/2019/07/5784b4194bbbf_o.jpg",margin: "0",price: "300zł",address: "Miasto ul.Ulica 1",size: "127 m2" , color: "white"};
@@ -9,7 +10,8 @@ const Home = () => {
   return(
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <Hero/>
-      <div className="grid-container" style= {{ width: '100%', justifyContent:'center', display: 'grid', gridTemplateColumns: 'repeat(3,300px)',gridTemplateRows: 'repeat(3,200px)', marginTop: '30px'}}>
+      <h5>See our latests listings</h5>
+      <div className="grid-container" style= {{ width: '100%', justifyContent:'center', display: 'grid', gridTemplateColumns: 'repeat(3,300px)',gridTemplateRows: 'repeat(2,200px)', marginTop: '30px'}}>
       <Listing {...myProps}/>
       <Listing {...myProps}/>
       <Listing {...myProps}/>
