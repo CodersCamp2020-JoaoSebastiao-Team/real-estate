94ef64acddc9b0d1fbacdaa1ade0d67a87ad0222
BartlomiejKaminski <75047014+BartlomiejKaminski@users.noreply.github.com>
2021-04-13 23:54:09 +0200

    callendar added

---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,87 @@
+import './index.scss';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker } from 'react-dates';
+
+class Calendar extends Component {
+    constructor(props: any){
+        super(props);
+        this.state = {
+            startDate: null,
+            endDate: null
+        }
+    }
+        startedDate = () => {
+         // @ts-ignore
+        alert((Math.abs(this.state.startDate - this.state.endDate))/(1000*3600*24)+1); 
+        }
+
+    render() {
+        return (
+            <div id="calendar_container">
+                <DateRangePicker
+                //@ts-ignore
+                startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                //@ts-ignore
+                endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                //@ts-ignore
+                focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                />
+                <button onClick={this.startedDate}>COST</button>
+            </div>
+        
+        )
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+// function Calendar() {
+    
+//     const startValue: Date = new Date(new Date().getFullYear(), new Date().getMonth());
+//     const minValue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+//     const endValue: Date = new Date(new Date().getFullYear(), new Date().getMonth());
+//     return (
+
+//         <div id="calendar_container">
+//             <DateRangePickerComponent placeholder="Which days you want to book ?"
+//             startDate = {startValue}
+//             endDate = {endValue}
+//             min = { minValue }
+            
+//             format="dd-MM-yy"></DateRangePickerComponent>
+//         </div>
+//     );
+// }
+
+export default Calendar;
--- src/pages/sell.tsx
+++ src/pages/sell.tsx
@@ -1,8 +1,15 @@
+import Calendar from '../components/calendar'
+import listingProps from '../components/listing-details/listingProps'
+
+
 const Sell = () => {
   return(
+  <div>
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <h2>Sell page</h2>
     </div>
+      <Calendar/>
+  </div>
   );
 };
 export default Sell;
