25da5abc6bc90481cae8e8879a7839bd5db6b7ff
Magdalena <sochamagdalena8@gmail.com>
2021-04-15 15:07:31 +0200

    style fixes

--- src/pages/buy.tsx
+++ src/pages/buy.tsx
@@ -21,7 +21,7 @@ const Buy = () => {
     maxFloorSpace: 999999,
   });
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(3);
+  const [postPerPage] = useState(6);
 
   let [loading, setLoading] = useState(true);
  
